7a874ea4ce923e318e63c7699303ddb9aa4e8700
zehuaz4 <46539586+blackpatton17@users.noreply.github.com>
2020-08-21 16:30:17 -0700

    update profile

--- js/profile.js
+++ js/profile.js
@@ -1,60 +1,18 @@
 "use strict";
 
-const URL = "https://mpvl0452tj.execute-api.us-east-1.amazonaws.com/Prod/user";
-
 window.addEventListener("load", () => {init()});
 
 const init = () => {
-    renderUserData().catch(error => {console.error("cannot render user data: ", error)});
-};
-
-const renderUserData = () => {
-    queryUserData()
-      .then(result => {
-          renderClassCard(result);
-      });
-};
-
-const queryUserData = () => {
-    let finalURL = `${URL}?user=testUser`;
-    return fetch(finalURL)
-        .then(response => {
-            if (response.status !== 200) {
-                return new Error(response.json().message);
-            }
-            return response.json();
-        })
-        .then(response => {
-            return response.result;
-        })
-        .catch(error => {
-            console.error("error: ", error);
+    qsa("#class-card-wrapper").forEach(x => {
+        x.addEventListener("click", (e) => {
+            window.location = `./Index.html?classTitle=${e.target.id}`;
         });
-};
-
-const renderClassCard = (result) => {
-    result.forEach(item => {
-        qs("#class-card-wrapper").appendChild(genClassCardComponent(item))
-    });
-};
-
-const genClassCardComponent = (data) => {
-    let card = crNewEl("div");
-    card.textContent = data;
-    card.classList.add("class-card");
-    card.addEventListener("click", () => {
-        window.location = `./Index.html?classTitle=${data}`;
-
-    });
-    return card;
+    })
 };
 
 /****************** helper functions *********************/
 
-const qs = (el) => {
-    return document.querySelector(el);
-};
 
-const crNewEl = (el) => {
-    return document.createElement(el);
+const qsa = (el) => {
+    return document.querySelectorAll(el);
 };
